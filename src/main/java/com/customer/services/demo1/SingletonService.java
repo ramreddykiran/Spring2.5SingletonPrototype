c762263ebb6a5edfbf249ca149ce0d792a724ba6
ramreddykiran <ramreddykiran@gmail.com>
2017-03-08 17:56:28 +0530

    committing the file missed in last commit

---
+++ src/main/java/com/customer/services/demo1/SingletonService.java
@@ -0,0 +1,18 @@
+package com.customer.services.demo1;
+
+public class SingletonService {
+	
+	private String singletonMessage;
+
+	public String getSingletonMessage() {
+		return singletonMessage;
+	}
+
+	public void setSingletonMessage(String singletonMessage) {
+		this.singletonMessage = singletonMessage;
+	}
+	
+	
+	
+
+}
