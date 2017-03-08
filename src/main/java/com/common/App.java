388c2c1bd1fd8428cc26d7703c6ebf389fb9fa8a
ramreddykiran <ramreddykiran@gmail.com>
2017-03-08 17:54:55 +0530

    Changed the structure of classes and packages to support multiple scenario examples

--- src/main/java/com/common/App.java
+++ src/main/java/com/common/App.java
@@ -3,21 +3,45 @@ package com.common;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.customer.services.CustomerService;
+import com.customer.services.demo1.PrototypeService;
+import com.customer.services.demo1.SingletonService;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	ApplicationContext context = 
-    		new ClassPathXmlApplicationContext(new String[] {"Spring-Customer.xml"});
+public class App {
+    public static void main( String[] args ) {
+    	ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"Spring-Customer.xml"});
+    	App app = new App();
+    	
+    	//This will demonstrate single class level scope
+    	app.demo1(context);
+    	
+    	//super class is singleton and subclass is prototype
+    	app.demo2(context);
+    }
 
-    	CustomerService custA = (CustomerService)context.getBean("customerService");
-    	custA.setMessage("Message by custA");
-    	System.out.println("Message : " + custA.getMessage());
+	private void demo2(ApplicationContext context) {
+		
+	}
+
+	private void demo1(ApplicationContext context) {
+		System.out.println("demo1 started");
+		PrototypeService prototypeService1 = (PrototypeService)context.getBean("prototypeService");
+    	prototypeService1.setPrototypeMessage("prototype service message");
+    	System.out.println("Prototype Message : " + prototypeService1.getPrototypeMessage());
     	
     	//retrieve it again
-    	CustomerService custB = (CustomerService)context.getBean("customerService");
-    	System.out.println("Message : " + custB.getMessage()); // it displays null as the spring scope is prototype
-    }
+    	PrototypeService prototypeService2 = (PrototypeService)context.getBean("prototypeService");
+    	
+    	System.out.println("Prototyppe Message : " + prototypeService2.getPrototypeMessage()); 
+    	// it displays null as the scope is prototype
+    	
+    	SingletonService singletonService1 = (SingletonService) context.getBean("singletonService");
+    	singletonService1.setSingletonMessage("singleton service message");
+    	System.out.println("singleton message "+singletonService1.getSingletonMessage());
+    	
+    	SingletonService singletonService2 = (SingletonService) context.getBean("singletonService");
+    	System.out.println("singleton message "+singletonService2.getSingletonMessage());
+    	//it displays singletonService1 message as the scope is singleton
+    	
+    	System.out.println("demo1 completed");
+	}
 }
---
+++ src/main/java/com/customer/services/demo1/PrototypeService.java
@@ -0,0 +1,13 @@
+package com.customer.services.demo1;
+
+public class PrototypeService {
+	private String prototypeMessage;
+	
+	public String getPrototypeMessage() {
+		return prototypeMessage;
+	}
+
+	public void setPrototypeMessage(String prototypeMessage) {
+		this.prototypeMessage = prototypeMessage;
+	}
+}
